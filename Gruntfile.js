6d814e6db398946807410385f79fb6d7e5320a4b
Adam Fratino <adamfratino@Adams-MacBook-Pro.local>
2014-09-01 20:56:11 -0400

    just fiddling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    htmlmin: {
+    // htmlmin: {
+    //   dist: {
+    //     options: {
+    //       removeComments: true,
+    //       collapseWhitespace: true,
+    //       link: true,
+    //       minifyCSS: true,
+    //       minifiyJS: true,
+    //     },
+    //     files: {
+    //       'index.html': 'index-src.html',
+    //     }
+    //   },
+    // },
+    sass: {
       dist: {
         options: {
-          removeComments: true,
-          collapseWhitespace: true,
-          link: true,
-          minifyCSS: true,
-          minifiyJS: true,
+          style: 'compressed'
         },
         files: {
-          'index.html': 'index-src.html',
+          'dist/css/main.min.css': 'src/scss/main.scss',
+          'vendor/css/bootstrap.min.css': 'vendor/scss/bootstrap.scss'
         }
-      },
-    },
-    sass: {
-        dist: {
-            options: {
-                style: 'compressed'
-            },
-            files: {
-                'dist/css/main.min.css': 'src/scss/main.scss',
-                'vendor/css/bootstrap.min.css': 'vendor/scss/bootstrap.scss'
-            }
-        } 
+      } 
     },
     jshint: {
       files: ['src/js/*.js'],
@@ -72,7 +72,7 @@ module.exports = function(grunt) {
       },
       html: {
         files: ['*.html','**/*.html'],
-        tasks: ['htmlmin', 'imagemin'],
+        tasks: ['imagemin'],
         options: {
           livereload: true,
         }
@@ -80,12 +80,12 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  // grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['sass', 'jshint', 'uglify', 'htmlmin', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'jshint', 'uglify', 'imagemin']);
 };
